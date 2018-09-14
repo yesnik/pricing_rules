14d1b10da78e12f7ef12043b16b7079179cb1e23
Nikita Nyatin <yesnik@yandex.ru>
2018-09-14 09:20:02 +0500

    Add tests for checkout

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -87,5 +87,50 @@ describe Checkout do
         end
       end
     end
+
+    context 'when CheaperItemRule provided' do
+      let(:pricing_rules) { [CheaperItemRule.new(tshirt, 3, 19.0)] }
+      subject { described_class.new(pricing_rules) }
+
+      context 'when we have items for single pricing rule' do
+        before do
+          allow(subject).to receive(:items).
+            and_return([tshirt, tshirt, tshirt, voucher, tshirt])
+        end
+
+        it 'returns price with discount applied' do
+          expect(subject.total_price).to eq 81.0
+        end
+      end
+    end
+
+    context 'when FreeItemRule and CheaperItemRule applied' do
+      let(:pricing_rules) do
+        [CheaperItemRule.new(tshirt, 3, 19.0), FreeItemRule.new(voucher, 2)]
+      end
+
+      subject { described_class.new(pricing_rules) }
+
+      context 'when we have items for 2 pricing rules' do
+        before do
+          allow(subject).to receive(:items).
+            and_return([voucher, tshirt, voucher, voucher, mug, tshirt, tshirt])
+        end
+
+        it 'returns price with discount applied' do
+          expect(subject.total_price).to eq 74.5
+        end
+      end
+
+      context 'when we do not have any items for pricing rules' do
+        before do
+          allow(subject).to receive(:items).and_return([voucher, tshirt, mug])
+        end
+
+        it 'returns price with discount applied' do
+          expect(subject.total_price).to eq 32.5
+        end
+      end
+    end
   end
 end
